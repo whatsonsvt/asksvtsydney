fb6051d95f4de5d9f49c32b36868424e1f2c1003
user01 <USERO1@USER.COM>
2019-06-10 05:20:12 +1000

    Added Date

--- chatbot.js
+++ chatbot.js
@@ -1,15 +1,35 @@
- 
-$(function () {
+ var me = {};
+me.avatar = "me.png";
+
+var you = {};
+you.avatar = "you.png";
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}            
+
+ $(function () {
     window.initialMessageDisplayed = false;
+	var date = formatAMPM(new Date());
     $(document).mouseenter(function(){
         if(!window.initialMessageDisplayed){
             var obj = JSON.parse($("#dom-target").text());
-            var event = obj.result.action;
+			var displayDate = '<p><small>'+date+'</small></p>';
+			var event = obj.result.action;
             var answerdiv = jQuery('<div/>', {
                 html: obj.result.fulfillment.speech.linkify()+'&nbsp;',
                 'class': "rounded-div-bot",
                 tabindex:1
             });
+			var displayDate = "<div class='rounded-div-bot'><small>" + date + "</small></div>";
+			answerdiv.append(displayDate);
             $("#chat-text").append(answerdiv);
             $("#message").focus();
             window.initialMessageDisplayed = true;
@@ -45,8 +65,11 @@ $(function () {
                     'class': "rounded-div-bot",
                     tabindex:1
                 });
+				
+				var displayDate = "<div class='rounded-div-bot'><small>" + date + "</small></div>";
+				answerdiv.append(displayDate);
                 $("#chat-text").append(answerdiv);
-                if(event){
+				 if(event){
                     var stylingDiv = jQuery('<div/>', {
                         html: $("#template").html(),
                         tabindex:1
@@ -98,9 +121,9 @@ if(!String.linkify) {
  
         return this
             .replace(urlPattern,
-                '<a class="answerLink" style="color:#0000EE" target="_blank" href="$&">$&</a>')
+                '<a class="answerLink" style="color:#0000EE" target="_blank" href="$&">click here</a>')
             .replace(pseudoUrlPattern,
-                '$1<a class="answerLink" style="color:#0000EE" target="_blank" href="http://$2">$2</a>')
+                '$1<a class="answerLink" style="color:#0000EE" target="_blank" href="http://$2">click here</a>')
             .replace(emailAddressPattern,
                 '<a class="answerLink" style="color:#0000EE" href="mailto:$&">$&</a>');
     };
